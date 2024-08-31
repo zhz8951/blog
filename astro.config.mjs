1d03c160e8a4c341d717123d7fc7e69839f5d1f9
zhz8951 <zhz89512021@outlook.com>
2024-08-31 09:25:19 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,8 +31,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
-  base: "/",
+  site: "https://blog.zhz8951.us.kg/",
   trailingSlash: "always",
   integrations: [
     tailwind(),
